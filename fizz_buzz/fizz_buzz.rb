485cd62b8e91582e63b44026d936f461185867bb
hakimu jackson <hakimuj@gmail.com>
2014-02-16 11:54:47 -0800

    Initial commit. Need refactor & tests

---
+++ fizz_buzz/fizz_buzz.rb
@@ -0,0 +1,23 @@
+# A number divisible by three replace with 'Fizz'
+# A number divisible by 5 replace with 'Buzz'
+# A number divisible by 3 & 5 replace with 'FizzBuzz'
+# Display the number for all other numbers
+
+class FizzBuzz
+
+	def self.checker(number)
+		if number % 5 == 0 && number % 3 == 0
+			"FizzBuzz"
+		elsif number % 3 == 0
+			"Fizz"
+		elsif number % 5 == 0
+			"Buzz"
+		else
+			number
+		end
+	end
+
+
+end
+
+puts FizzBuzz.checker(15)
